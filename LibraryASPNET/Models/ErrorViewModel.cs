0d9c745a57fc8dfa7c5696a7c1c91fccba3b8bdd
BilalMO <azbilal.motassim@gmail.com>
2023-12-12 18:37:19 +0100

    Add project files.

---
+++ LibraryASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
