bea5688ae07614a89fbe7ea26d14ab5eb0d1066c
Hamza-Elbouzidi <elbouzidihamza5@gmail.com>
2023-12-22 22:56:13 +0100

    Database Link,signup view with the account creation

---
+++ LibraryASPNET/Models/User/User.cs
@@ -0,0 +1,26 @@
+﻿namespace LibraryASPNET.Models.User;
+
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    public int Id { get; set; }
+
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    public bool IsAdmin { get; set; }
+}
