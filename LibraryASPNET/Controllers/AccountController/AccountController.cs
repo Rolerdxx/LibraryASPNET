6ee1e9dc6e1c23824a23c31d8800c19914605a52
Hamza-Elbouzidi <elbouzidihamza5@gmail.com>
2023-12-24 20:39:21 +0100

    updates about signup page

--- LibraryASPNET/Controllers/AccountController/AccountController.cs
+++ LibraryASPNET/Controllers/AccountController/AccountController.cs
@@ -12,23 +12,41 @@ public class AccountController : Controller
     }
 
     [HttpGet]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+   
     [HttpPost]
+    [HttpGet]
     public IActionResult SignUp(User user)
     {
+        var existingUser = _context.users.FirstOrDefault(u => u.Email == user.Email);
         if (ModelState.IsValid)
         {
+             if (string.IsNullOrWhiteSpace(user.FirstName) || string.IsNullOrWhiteSpace(user.LastName) ||
+                string.IsNullOrWhiteSpace(user.Email) || string.IsNullOrWhiteSpace(user.Password))
+             {
+                    TempData["SignUpErrorMessage"] = "All the inputs should be filled";
+                        return View("SignUp");
+             } else if (existingUser != null)
+             {
+                    TempData["SignUpErrorMessage"] = "An account with this email already exists.";
+                        return View("SignUp");
+                
+             }
             _context.users.Add(user);
             _context.SaveChanges();
 
-            TempData["SignUpMessage"] = "Account created successfully!";
+                    TempData["SignUpMessage"] = "Account created successfully!";
+                         return View("SignUp");
         }
 
-        return View();
+        return View(user);
     }
-
 
     public IActionResult SignUpSuccess()
     {
-        return View(); 
+        return View();
     }
 }
