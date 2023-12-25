e93849dfce62d55d66f83da841578c0416f01dac
Salma IDMANSOUR <idmansour.sa@gmail.com>
2023-12-25 22:06:02 +0100

    add book model and controller, (GetOneBook and GetAllBooks) actions

---
+++ LibraryASPNET/Models/Book/Book.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryASPNET.Models.Book
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public string Available { get; set; }
+
+    }
+}
